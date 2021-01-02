7a053b2e41e46cfe1069c70e217a9da6f5efc87b
Natanael Arndt <arndtn@gmail.com>
2021-01-02 14:36:20 +0100

    Add test for empty publicID with Turtle

--- test/test_empty_publicID.py
+++ test/test_empty_publicID.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from rdflib.term import URIRef
 
 
-def test_empty_string_for_publicID_when_loading_a_file():
+def test_empty_string_for_publicID_when_loading_an_rdfxml_file():
     ''' Tests that we can pass in an empty string to publicID when
-        parsing from a file name. since '' is falsy, it could be
+        parsing from an RDF/XML file name. since '' is falsy, it could be
         treated as None (e.g. if not publicID). '''
 
     xml_sample = """\
@@ -21,6 +21,30 @@ def test_empty_string_for_publicID_when_loading_a_file():
     with tempfile.TemporaryDirectory() as td:
         sample_file = str(Path(td) / "sample.xml")
         open(sample_file, 'w').write(xml_sample)
+
+        g.parse(sample_file, publicID="")
+
+    subject, predicate, object = next(iter(g))
+
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert object == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
+
+
+def test_empty_string_for_publicID_when_loading_a_ttl_file():
+    ''' Tests that we can pass in an empty string to publicID when
+        parsing from a turtle file name. since '' is falsy, it could be
+        treated as None (e.g. if not publicID). '''
+
+    ttl_sample = """\
+    <#_AB16765A-B19E-4454-A58F-868D23C6CD26> a
+    <http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo> .
+    """
+
+    g = rdflib.Graph()
+    with tempfile.TemporaryDirectory() as td:
+        sample_file = str(Path(td) / "sample.ttl")
+        open(sample_file, 'w').write(ttl_sample)
 
         g.parse(sample_file, publicID="")
 
